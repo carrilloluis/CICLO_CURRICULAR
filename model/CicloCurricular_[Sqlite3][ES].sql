604932994c89267690aa279d93a4f92f41dbf2ca
Luis Carrillo <lois@localhost.localdomain>
2024-08-31 19:15:14 -0400

    Second Update

--- model/CicloCurricular_[Sqlite3][ES].sql
+++ model/CicloCurricular_[Sqlite3][ES].sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS [Ciclo Curricular] (
 	[id] CHAR(36) NOT NULL PRIMARY KEY,
 	[fecha de vigencia] DATE NOT NULL, 
 	[ciclo numérico] INTEGER NOT NULL,
+	-- **Notación** del Ciclo Curricular
 	[descripción] TEXT NOT NULL,
 	[estados] INTEGER NOT NULL
 ) WITHOUT ROWID;
