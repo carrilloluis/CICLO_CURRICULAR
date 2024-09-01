2cb25780d99ce6d137af07e143410c280f5e22d4
Luis Carrillo <lois@localhost.localdomain>
2024-09-01 19:08:09 -0400

    Update data

--- main.py
+++ main.py
@@ -21,18 +21,29 @@ def asset_files(filepath):
 def error404(error):
 	return 'Nothing here, sorry'
 
-
 @app.route('/CICLO_CURRICULAR/v1/', method='GET')
 def get_courses():
 	try:
 		with sqlite3.connect(DB_PATH) as connection:
 			with closing(connection.cursor()) as cursor:
-				cursor.execute("SELECT UPPER([id]) AS id, UPPER([descripción]) AS nm, (([estados] & 1) = 1) AS e FROM [Ciclo Curricular]", ())
+				cursor.execute("SELECT UPPER([id]) AS id, UPPER([descripción]) AS nm FROM [Ciclo Curricular] WHERE ([estados] & 1)=1 ORDER BY [ciclo numérico]", ())
 				cursor.row_factory = sqlite3.Row
 				ds_ = [dict(r) for r in cursor.fetchall()]
 				return bottle.HTTPResponse(body=json.dumps({'data': ds_}), status=200, headers=MINIMAL_CORS)
 	except sqlite3.OperationalError as e:
 		return bottle.HTTPResponse(body=json.dumps({'msg': str(e)}), status=500)
 
+@app.route('/CICLO_CURRICULAR/v1/_/', method='GET')
+def get_courses():
+	try:
+		with sqlite3.connect(DB_PATH) as connection:
+			with closing(connection.cursor()) as cursor:
+				cursor.execute("SELECT UPPER([id]) AS id, UPPER([descripción]) AS nm, (([estados] & 1) = 1) AS st FROM [Ciclo Curricular] ORDER BY [ciclo numérico]", ())
+				cursor.row_factory = sqlite3.Row
+				ds_ = [dict(r) for r in cursor.fetchall()]
+				return bottle.HTTPResponse(body=json.dumps({'data': ds_}), status=200, headers=MINIMAL_CORS)
+	except sqlite3.OperationalError as e:
+		return bottle.HTTPResponse(body=json.dumps({'msg': str(e)}), status=500)
+		
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=8093, reloader=True, debug=True)
